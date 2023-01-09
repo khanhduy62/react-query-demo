2315d5544234cbe1b26e8aa1fb1b77583d737e2d
Duy Le <duyle@hmsfirst.com>
2023-01-09 15:36:39 +0700

    data transformation

--- src/components/RQSuperHerosPage.jsx
+++ src/components/RQSuperHerosPage.jsx
@@ -10,7 +10,15 @@ const RQSuperHeroesPage = () => {
   const { isLoading, data, error, isError, isFetching } = useQuery(
     ['super-heroes', id],
     () => fetchSuperHeroes(id),
-    { retry: 0, refetchOnMount: false }
+    { 
+      retry: 0,
+      select: (data) => {
+        if (data?.data && Array.isArray(data.data)) {
+          return data.data.map(el => el.name)
+        }
+        return data?.data?.name || null;
+      }
+    }
   );
 
   console.log({ isFetching, isLoading, data });
@@ -24,13 +32,13 @@ const RQSuperHeroesPage = () => {
       <h2>RQ Super Heroes Page</h2>
       {isLoading && <h2>Loading...</h2>}
       <input onChange={(e) => setId(e.nativeEvent.target.value)} />
-      {data?.data ? (
-        Array.isArray(data.data) ? (
-          data?.data?.map((hero) => {
-            return <div key={hero.name}>{hero.name}</div>;
+      {data ? (
+        Array.isArray(data) ? (
+          data?.map((hero) => {
+            return <div key={hero}>{hero}</div>;
           })
         ) : (
-          <div>{data.data.name}</div>
+          <div>{data}</div>
         )
       ) : null}
     </>
